4a533bf3bbc48fe00b06e90883a5b9cc52a90468
guategeek <user@guategeek.com>
2024-05-31 13:15:05 -0600

    Moved and Renamed my dactly flexdemot to the mantis

---
+++ keyboards/handwired/g2_mantis/mantis_v1/rules.mk
@@ -0,0 +1,17 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+SPLIT_KEYBOARD = yes
+OLED_ENABLE = yes
+LTO_ENABLE = yes			# saves memory, takes more time to compile
+
+# BOOTLOADER = qmk-dfu # As I am trying to get my pro micros out of Catarina

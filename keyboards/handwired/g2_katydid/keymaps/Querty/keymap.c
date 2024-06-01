d1018959ef74f85f5f52d093056f0bde90da87e1
guategeek <user@guategeek.com>
2024-06-01 11:51:46 -0600

    Finaly got it to compile!

--- keyboards/handwired/g2_katydid/keymaps/Querty/keymap.c
+++ keyboards/handwired/g2_katydid/keymaps/Querty/keymap.c
@@ -26,7 +26,7 @@
 
 enum Quertylayers {
   _QWERTY,
-  _NUMPAD,
+  _NAV,
 };
 
 // enum custom_keycodes {
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q   , KC_W    , KC_E   , KC_R   , KC_T   ,                          KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
         KC_CAPS, KC_A   , KC_S    , KC_D   , KC_F   , KC_G   ,                          KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z   , KC_X    , KC_C   , KC_V   , KC_B   ,                          KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-        TG_NAV , QK_GESC, LOWER   , KC_LEFT, KC_RGHT,                                            KC_UP  , KC_DOWN, KC_LBRC, KC_RBRC, _______,
+        TG_NAV , QK_GESC, KC_BSLS , KC_LEFT, KC_RGHT,                                            KC_UP  , KC_DOWN, KC_LBRC, KC_RBRC, _______,
                                                       KC_LGUI, KC_LALT,        KC_RCTL, KC_RGUI,
                                              KC_BSPC, KC_DEL , KC_HOME,        KC_PGUP, KC_ENT , KC_SPC ,
                                                                KC_END ,        KC_PGDN
